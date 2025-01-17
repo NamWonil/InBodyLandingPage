604e2841d8d6895f58ed5e157cb8feac348ae195
南园日 <nanyuanri@inbodychina.com>
2025-01-17 16:06:03 +0800

    a

--- InBodyLandingPage/vite.config.ts
+++ InBodyLandingPage/vite.config.ts
@@ -11,24 +11,11 @@ export default defineConfig({
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
 	},
-	// css: {
-	// 	postcss: {
-	// 		plugins: [
-	// 			postcssPxtorem({
-	// 				rootValue: 16, // 基准值，可以根据设计稿的字体大小调整
-	// 				propList: ['*'], // 转换所有属性的 px 单位
-	// 				selectorBlackList: [], // 不转换的选择器，空数组表示没有排除的选择器
-	// 				replace: true, // 是否替换原 px 为 rem
-	// 				mediaQuery: false, // 是否转换媒体查询中的 px
-	// 				minPixelValue: 2 // 小于此值的 px 不转换
-	// 			})
-	// 		]
-	// 	}
-	// },
 	optimizeDeps: {
 		include: ['vue-i18n'] // 确保 vue-i18n 被正确优化
 	},
 	server: {
 		host: '0.0.0.0'
-	}
+	},
+	base: 'InBodyLandingPage'
 })
